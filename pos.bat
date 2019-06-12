060634e84fcf8a5cc38c12f9cb4f22afa0e81a63
Arc Zee <z@arcz.ee>
2019-06-12 15:10:54 +0800

    nth

--- pos.bat
+++ pos.bat
@@ -4,9 +4,17 @@
 set pos_exe=MTpos_jh.exe
 set pos_bat=pos.bat
 set "pos_path=d:\KSOA POS\"
+set "update_pos_bat=\\192.168.10.55\s\src\update_pos.bat"
+
+:: Is Windows XP?
+ver | find "5.1" > nul 
+if %errorlevel% equ 0 (
 set "pos_lnk_old=%USERPROFILE%\×ÀÃæ\%pos_exe%.lnk"
 set "pos_lnk=%USERPROFILE%\×ÀÃæ\%pos_bat%.lnk"
-set "update_pos_bat=\\192.168.10.55\s\src\update_pos.bat"
+) else (
+set "pos_lnk_old=%USERPROFILE%\Desktop\%pos_exe%.lnk"
+set "pos_lnk=%USERPROFILE%\Desktop\%pos_bat%.lnk"
+)
 
 d:
 cd "%pos_path%"
@@ -16,4 +24,4 @@ cd "%pos_path%"
 %update_pos_bat%
 
 :: run pos
-::start %pos_exe%
+start %pos_exe%
